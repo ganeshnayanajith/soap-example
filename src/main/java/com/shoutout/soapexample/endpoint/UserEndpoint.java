5c5e48c6d545004839bf30aa1b5a9d51badc876c
ganeshnayanajith <ganeshnayanajith40@gmail.com>
2020-02-19 17:06:32 +0530

    first commit

---
+++ src/main/java/com/shoutout/soapexample/endpoint/UserEndpoint.java
@@ -0,0 +1,27 @@
+package com.shoutout.soapexample.endpoint;
+
+import com.shoutout.soap_example.GetUserRequest;
+import com.shoutout.soap_example.GetUserResponse;
+import com.shoutout.soapexample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class UserEndpoint {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PayloadRoot(namespace = "http://shoutout.com/soap-example",
+            localPart = "getUserRequest")
+    @ResponsePayload
+    public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request) {
+        GetUserResponse response = new GetUserResponse();
+        response.setUser(userService.getUsers(request.getName()));
+        return response;
+    }
+}
